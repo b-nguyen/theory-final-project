54db7c28cc7ea6fe6aa9ccb1c5f1e9416ac19021
Brian Nguyen <btn3gj@virginia.edu>
2015-05-07 00:22:17 -0400

    Samurai imported GUI correct

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -10,6 +10,7 @@ import java.awt.event.MouseEvent;
 import java.io.File;
 
 import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -28,13 +29,14 @@ public class MainPanel extends JPanel {
 	private SudokuGrid grid = null;
 	private SamuraiGrid samGrid = null;
 	private boolean samurai = false;
-	
+
 	JButton importInputButton = new JButton("Import Puzzle");
 	JButton createNewInputButton = new JButton("Create New Input");
-	JRadioButton normalGrid = new JRadioButton("Normal");
-	JRadioButton samuraiGrid = new JRadioButton("Samurai");
+	JRadioButton normalGridButton = new JRadioButton("Normal");
+	JRadioButton samuraiGridButton = new JRadioButton("Samurai");
 	JButton solveButton = new JButton("SOLVE");
 	JPanel buttonPanel = new JPanel();
+	ButtonGroup radioButtonGroup = new ButtonGroup();
 
 	// here my main gui has a reference to the JDialog and to the
 	// MyDialogPanel which is displayed in the JDialog
@@ -43,19 +45,22 @@ public class MainPanel extends JPanel {
 	private SolutionsDialogPanel solutionsDialogPanel = new SolutionsDialogPanel();
 	private JDialog dialog;
 	private JPanel gridPanel = new JPanel();
-	
+
 	public MainPanel() {
 		setLayout(new BorderLayout(0, 0));
-		
+
 		buttonPanel.setBackground(UIManager.getColor("Slider.background"));
 		add(buttonPanel, BorderLayout.NORTH);
 		buttonPanel.add(importInputButton);
 		buttonPanel.add(createNewInputButton);
-		normalGrid.setSelected(true);
-		buttonPanel.add(normalGrid);
-		samuraiGrid.setSelected(false);
-		buttonPanel.add(samuraiGrid);
-		
+		radioButtonGroup.add(normalGridButton);
+		radioButtonGroup.add(samuraiGridButton);
+		normalGridButton.setSelected(true);
+		samuraiGridButton.setSelected(false);
+		buttonPanel.add(normalGridButton);
+		buttonPanel.add(samuraiGridButton);
+
+
 		gridPanel.setBackground(UIManager.getColor("Slider.shadow"));
 		add(gridPanel, BorderLayout.CENTER);
 
@@ -72,28 +77,30 @@ public class MainPanel extends JPanel {
 				}
 			}
 		});
-		
+
 		solveButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				if (!samurai) {
 					solveNormalPuzzle();
+				} else {
+					solveSamuraiPuzzle();
 				}
 			}
 		});
-		
-		normalGrid.addMouseListener(new MouseAdapter() {
+
+		normalGridButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				samuraiGrid.setSelected(false);
+				//samuraiGridButton.setSelected(false);
 				samurai = false;
 			}
 		});
-		
-		samuraiGrid.addMouseListener(new MouseAdapter() {
+
+		samuraiGridButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				normalGrid.setSelected(false);
+				//normalGridButton.setSelected(false);
 				samurai = true;
 			}
 		});
@@ -104,7 +111,7 @@ public class MainPanel extends JPanel {
 		sizeDialogPanel = new SizeDialogPanel();
 		dialog = null;
 		fileDialogPanel = null;
-		
+
 		if (dialog == null) {
 			Window win = SwingUtilities.getWindowAncestor(this);
 			if (win != null) {
@@ -118,7 +125,11 @@ public class MainPanel extends JPanel {
 		dialog.setVisible(true); // here the modal dialog takes over
 
 		fileDialogPanel = new FileDialogPanel();
-		inputFile = fileDialogPanel.getFile();
+		try {
+			inputFile = fileDialogPanel.getFile();
+		} catch (IllegalArgumentException e) {
+			System.out.println("File not found");
+		}
 		size = sizeDialogPanel.getValue();
 		createGridNormal(size, inputFile);
 		solveButton.setEnabled(true);
@@ -129,46 +140,51 @@ public class MainPanel extends JPanel {
 	public void createGridNormal(int size, File inputFile) {
 		grid = new SudokuGrid(size);
 		grid.readInputFile(inputFile);
-		
+
 		int componentSize = (int) Math.sqrt(size);
 		gridPanel.removeAll();
-		
+
 		gridPanel.setLayout(new GridLayout(componentSize, componentSize, 0, 0));
 
 		for (int i = 0; i < size; i++) {
 			JPanel subGridPanel = new JPanel();
 			subGridPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-			subGridPanel.setLayout(new GridLayout(componentSize, componentSize, 0, 0));
+			subGridPanel.setLayout(new GridLayout(componentSize, componentSize,
+					0, 0));
 			gridPanel.add(subGridPanel);
-			
+
 			int cornerCol = (i % 3) * 3;
 			int cornerRow = i;
-			
-			int start_x = ((int) cornerRow/ componentSize)*componentSize;
-			int start_y = ((int) cornerCol / componentSize)*componentSize;
-			int end_x = (((int) cornerRow/componentSize))*componentSize + componentSize - 1;
-			int end_y = (((int) cornerCol/componentSize))*componentSize + componentSize - 1;
-			
+
+			int start_x = ((int) cornerRow / componentSize) * componentSize;
+			int start_y = ((int) cornerCol / componentSize) * componentSize;
+			int end_x = (((int) cornerRow / componentSize)) * componentSize
+					+ componentSize - 1;
+			int end_y = (((int) cornerCol / componentSize)) * componentSize
+					+ componentSize - 1;
+
 			for (int j = start_x; j <= end_x; j++) {
 				for (int k = start_y; k <= end_y; k++) {
 					JPanel smallGridPanel = new JPanel();
-					smallGridPanel.setBorder(new LineBorder(new Color(0, 0, 0), 1));
+					smallGridPanel.setBorder(new LineBorder(new Color(0, 0, 0),
+							1));
 					smallGridPanel.setLayout(new GridLayout(0, 1, 0, 0));
 					if (grid.getGrid()[j][k] != 0) {
-						JLabel label = new JLabel("" + grid.getGrid()[j][k], null, JLabel.CENTER);
-						smallGridPanel.add(label);		
+						JLabel label = new JLabel("" + grid.getGrid()[j][k],
+								null, JLabel.CENTER);
+						smallGridPanel.add(label);
 					}
 					subGridPanel.add(smallGridPanel);
 				}
 			}
 		}
 	}
-	
+
 	public void solveNormalPuzzle() {
 		solutionsDialogPanel = new SolutionsDialogPanel();
 		solutionsDialogPanel.createSolutions(grid, size);
 		dialog = null;
-		
+
 		if (dialog == null) {
 			Window win = SwingUtilities.getWindowAncestor(this);
 			if (win != null) {
@@ -180,9 +196,12 @@ public class MainPanel extends JPanel {
 			}
 		}
 		dialog.setVisible(true); // here the modal dialog takes over
-		
 	}
 	
+	public void solveSamuraiPuzzle() {
+		
+	}
+
 	public void readFromUserSamurai() {
 		fileDialogPanel = new FileDialogPanel();
 		inputFile = fileDialogPanel.getFile();
@@ -195,9 +214,9 @@ public class MainPanel extends JPanel {
 	public void createGridSamurai(File inputFile) {
 		samGrid = new SamuraiGrid();
 		samGrid.readInputFile(inputFile);
-		
+
 		gridPanel.removeAll();
-		
+
 		gridPanel.setLayout(new GridLayout(7, 7, 0, 0));
 
 		SudokuGrid leftTopGrid = samGrid.getGrids()[0];
@@ -205,53 +224,152 @@ public class MainPanel extends JPanel {
 		SudokuGrid middleGrid = samGrid.getGrids()[2];
 		SudokuGrid leftBottomGrid = samGrid.getGrids()[3];
 		SudokuGrid rightBottomGrid = samGrid.getGrids()[4];
-		
+
 		for (int i = 0; i < 49; i++) {
 			JPanel subGridPanel = new JPanel();
 			subGridPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
 			subGridPanel.setLayout(new GridLayout(3, 3, 0, 0));
 			gridPanel.add(subGridPanel);
-			
-			for (int j = 0; j < 9; j++) {
-				JPanel subSubGridPanel = new JPanel();
-				if (i != 3 && i != 10 && i != 21 && i != 22 && i != 26 && i != 27 && i != 38 && i != 45) {
-					subSubGridPanel.setBorder(new LineBorder(new Color(0, 0, 0), 1));
-				} else {
-					subSubGridPanel.setBorder(BorderFactory.createEmptyBorder());
+
+			// for (int j = 0; j < 9; j++) {
+
+			// Do not put in borders if it is suppose to be empty
+			if (i != 3 && i != 10 && i != 21 && i != 22 && i != 26 && i != 27
+					&& i != 38 && i != 45) {
+				// For top left grid
+				if (((i % 7 == 0) || (i % 7 == 1) || (i % 7 == 2)) && (i < 17)
+						&& (i != 16)) {
+					int cornerCol = (i % 7) * 3;
+					int cornerRow = (i / 7) * 3;
+					int start_x = ((int) cornerRow / 3) * 3;
+					int start_y = ((int) cornerCol / 3) * 3;
+					int end_x = (((int) cornerRow / 3)) * 3 + 3 - 1;
+					int end_y = (((int) cornerCol / 3)) * 3 + 3 - 1;
+
+					for (int k = start_x; k <= end_x; k++) {
+						for (int l = start_y; l <= end_y; l++) {
+							JPanel subSubGridPanel = new JPanel();
+							subSubGridPanel.setBorder(new LineBorder(new Color(
+									0, 0, 0), 1));
+							subSubGridPanel
+									.setLayout(new GridLayout(0, 1, 0, 0));
+							if (leftTopGrid.getGrid()[k][l] != 0) {
+								JLabel label = new JLabel(""
+										+ leftTopGrid.getGrid()[k][l], null,
+										JLabel.CENTER);
+								subSubGridPanel.add(label);
+							}
+							subGridPanel.add(subSubGridPanel);
+						}
+					}
+					// For top right grid
+				} else if (((i % 7 == 4) || (i % 7 == 5) || (i % 7 == 6))
+						&& (i < 21) && (i != 18)) {
+					int cornerCol = ((i % 7) - 4) * 3;
+					int cornerRow = ((i / 7)) * 3;
+					int start_x = ((int) cornerRow / 3) * 3;
+					int start_y = ((int) cornerCol / 3) * 3;
+					int end_x = (((int) cornerRow / 3)) * 3 + 3 - 1;
+					int end_y = (((int) cornerCol / 3)) * 3 + 3 - 1;
+
+					for (int k = start_x; k <= end_x; k++) {
+						for (int l = start_y; l <= end_y; l++) {
+							JPanel subSubGridPanel = new JPanel();
+							subSubGridPanel.setBorder(new LineBorder(new Color(
+									0, 0, 0), 1));
+							subSubGridPanel
+									.setLayout(new GridLayout(0, 1, 0, 0));
+							if (rightTopGrid.getGrid()[k][l] != 0) {
+								JLabel label = new JLabel(""
+										+ rightTopGrid.getGrid()[k][l], null,
+										JLabel.CENTER);
+								subSubGridPanel.add(label);
+							}
+							subGridPanel.add(subSubGridPanel);
+						}
+					}
+					// For bottom left grid
+				} else if (((i % 7 == 0) || (i % 7 == 1) || (i % 7 == 2))
+						&& (i < 45) && (i > 27) && (i != 30)) {
+
+					int cornerCol = (i % 7) * 3;
+					int cornerRow = ((i / 7) - 4) * 3;
+					int start_x = ((int) cornerRow / 3) * 3;
+					int start_y = ((int) cornerCol / 3) * 3;
+					int end_x = (((int) cornerRow / 3)) * 3 + 3 - 1;
+					int end_y = (((int) cornerCol / 3)) * 3 + 3 - 1;
+
+					for (int k = start_x; k <= end_x; k++) {
+						for (int l = start_y; l <= end_y; l++) {
+							JPanel subSubGridPanel = new JPanel();
+							subSubGridPanel.setBorder(new LineBorder(new Color(
+									0, 0, 0), 1));
+							subSubGridPanel
+									.setLayout(new GridLayout(0, 1, 0, 0));
+							if (leftBottomGrid.getGrid()[k][l] != 0) {
+								JLabel label = new JLabel(""
+										+ leftBottomGrid.getGrid()[k][l], null,
+										JLabel.CENTER);
+								subSubGridPanel.add(label);
+							}
+							subGridPanel.add(subSubGridPanel);
+						}
+					}
+					// For bottom right grid
+				} else if (((i % 7 == 4) || (i % 7 == 5) || (i % 7 == 6))
+						&& (i > 31) && (i != 32)) {
+					int cornerCol = ((i % 7) - 4) * 3;
+					int cornerRow = ((i / 7) - 4) * 3;
+					int start_x = ((int) cornerRow / 3) * 3;
+					int start_y = ((int) cornerCol / 3) * 3;
+					int end_x = (((int) cornerRow / 3)) * 3 + 3 - 1;
+					int end_y = (((int) cornerCol / 3)) * 3 + 3 - 1;
+
+					for (int k = start_x; k <= end_x; k++) {
+						for (int l = start_y; l <= end_y; l++) {
+							JPanel subSubGridPanel = new JPanel();
+							subSubGridPanel.setBorder(new LineBorder(new Color(
+									0, 0, 0), 1));
+							subSubGridPanel
+									.setLayout(new GridLayout(0, 1, 0, 0));
+							if (rightBottomGrid.getGrid()[k][l] != 0) {
+								JLabel label = new JLabel(""
+										+ rightBottomGrid.getGrid()[k][l],
+										null, JLabel.CENTER);
+								subSubGridPanel.add(label);
+							}
+							subGridPanel.add(subSubGridPanel);
+						}
+					}
+				} else if ((i % 7 == 2) || (i % 7 == 3) || (i % 7) == 4) {
+					int cornerCol = ((i % 7) - 2) * 3;
+					int cornerRow = ((i / 7) - 2) * 3;
+					int start_x = ((int) cornerRow / 3) * 3;
+					int start_y = ((int) cornerCol / 3) * 3;
+					int end_x = (((int) cornerRow / 3)) * 3 + 3 - 1;
+					int end_y = (((int) cornerCol / 3)) * 3 + 3 - 1;
+
+					for (int k = start_x; k <= end_x; k++) {
+						for (int l = start_y; l <= end_y; l++) {
+							JPanel subSubGridPanel = new JPanel();
+							subSubGridPanel.setBorder(new LineBorder(new Color(
+									0, 0, 0), 1));
+							subSubGridPanel
+									.setLayout(new GridLayout(0, 1, 0, 0));
+							if (middleGrid.getGrid()[k][l] != 0) {
+								JLabel label = new JLabel(""
+										+ middleGrid.getGrid()[k][l], null,
+										JLabel.CENTER);
+								subSubGridPanel.add(label);
+							}
+							subGridPanel.add(subSubGridPanel);
+						}
+					}
 				}
-				subGridPanel.add(subSubGridPanel);
 			}
 		}
-		/*
-		for (int i = 0; i < size; i++) {
-			JPanel subGridPanel = new JPanel();
-			subGridPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-			subGridPanel.setLayout(new GridLayout(componentSize, componentSize, 0, 0));
-			gridPanel.add(subGridPanel);
-			
-			int cornerCol = (i % 3) * 3;
-			int cornerRow = i;
-			
-			int start_x = ((int) cornerRow/ componentSize)*componentSize;
-			int start_y = ((int) cornerCol / componentSize)*componentSize;
-			int end_x = (((int) cornerRow/componentSize))*componentSize + componentSize - 1;
-			int end_y = (((int) cornerCol/componentSize))*componentSize + componentSize - 1;
-			
-			for (int j = start_x; j <= end_x; j++) {
-				for (int k = start_y; k <= end_y; k++) {
-					JPanel smallGridPanel = new JPanel();
-					smallGridPanel.setBorder(new LineBorder(new Color(0, 0, 0), 1));
-					smallGridPanel.setLayout(new GridLayout(0, 1, 0, 0));
-					if (grid.getGrid()[j][k] != 0) {
-						JLabel label = new JLabel("" + grid.getGrid()[j][k], null, JLabel.CENTER);
-						smallGridPanel.add(label);		
-					}
-					subGridPanel.add(smallGridPanel);
-				}
-			}
-		}*/
 	}
-	
+
 	public int getInputSize() {
 		return this.size;
 	}
